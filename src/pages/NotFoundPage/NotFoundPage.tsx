5c25cdaaf241f53c2d83ff963c4447678c87d169
Victoria Sokolvak <sokolvak.victoria@gmail.com>
2025-03-27 12:44:38 +0200

    feat: add NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,5 +1,21 @@
+import { Logo } from '../../components/Logo';
+import styles from './NotFoundPage.module.css';
+
 export const NotFoundPage = () => {
   return (
-    <div>NotFoundPage</div>
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Logo />
+        <div className={styles.header}>
+          <h2 className={styles.title}>
+            Oops! Page
+            <span className={styles.title_highlighted}> not found</span>
+          </h2>
+          <p className={styles.text}>
+            The page you’re looking for doesn’t exist. Try checking the URL or go back to the homepage.
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
